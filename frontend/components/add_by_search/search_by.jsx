2ecde8616a17f7a4b6b3a62111c57750cf9c4068
Zalkifl Syed <zcksyed3@gmail.com>
2019-01-29 20:10:26 -0500

    Fix modal issue's

--- frontend/components/add_by_search/search_by.jsx
+++ frontend/components/add_by_search/search_by.jsx
@@ -6,18 +6,19 @@ class SearchBy extends React.Component {
         super(props);
 
         this.state = {
-            field: "",
+            searchTerm: "",
             body: ""
         };
 
         this.handleSubmit = this.handleSubmit.bind(this); 
+        this.fillForm = this.fillForm.bind(this);
     }
 
     handleSubmit(e) {
         e.preventDefault();
         this.props.createFriend(this.state);
         this.setState = {
-            field: "",
+            searchTerm: "",
             body: ""
         };
     }
@@ -33,10 +34,10 @@ class SearchBy extends React.Component {
             <div className="Search-me">
                 <h1><img className="inv-img" src={window.flavicon}/>Invite friends</h1>
                 <div>
-                    <form onSubmit={this.handleSubmit(e)}>
-                        <input className="inv-user-field"  onChange={this.fillForm('field')} type="text" value={`To: ${this.props.top}`}/>
+                    <form onSubmit={this.handleSubmit}>
+                        <input className="inv-user-field"  onChange={this.fillForm('searchTerm')} type="text" value=""/>
 
-                        <input className="inv-message" onChange={this.fillForm('body')} type="text-area" value="Include an optional message"/>
+                        <input className="inv-message" onChange={this.fillForm('body')} type="text-area" value="" placeholder="Include an optional message" />
 
                         <input className="btn inv-friends-btn" type="submit" value="Send invites and add friends" />
                     </form>
