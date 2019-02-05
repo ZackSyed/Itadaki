2c26b316055e6816c37b4552c59f247e819c61ae
Zalkifl <zcksyed3@gmail.com>
2019-02-05 16:53:28 -0500

    add Tab model

--- app/models/tab.rb
+++ app/models/tab.rb
@@ -1,3 +1,9 @@
 class Tab < ApplicationRecord 
-    
+    validates :total, presence: true 
+    validates :settled, inclusion: { in: [ true, false ] }
+
+    belongs_to :group 
+    belongs_to :lender
+        foreign_key: :lender_id
+        class_name: :User
 end 
