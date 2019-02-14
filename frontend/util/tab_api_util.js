22f61ebe91c6c66b18139f37932e9e56a441c084
Zalkifl <zcksyed3@gmail.com>
2019-02-14 17:37:14 -0500

    Add Ajax request for tabs

--- frontend/util/tab_api_util.js
+++ frontend/util/tab_api_util.js
@@ -1,5 +1,15 @@
 export const fetchTab = (id) => {
     return $.ajax({
-        method
-    })
+        method: 'get',
+        url: `api/tabs/${id}`, 
+        data: { tab }
+    });
+}
+
+export const createTab = (tab) => {
+    return $.ajax({
+        method: 'post',
+        url: `api/tabs`,
+        data: { tab }
+    });
 }
